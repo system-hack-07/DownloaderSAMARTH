7a70b8b9bedb52ad436621e4f78f69582c1c5c6e
system-hack-07 <nandoskarsamarth@gmail.com>
2026-05-22 12:39:29 +0530

    Simplify error response in download.js

--- api/download.js
+++ api/download.js
@@ -15,9 +15,6 @@ export default async function handler(req, res) {
     res.status(200).json(data);
 
   } catch (err) {
-    res.status(500).json({
-      error: "Server error",
-      details: err.message
-    });
+    res.status(500).json({ error: "Server error" });
   }
 }
